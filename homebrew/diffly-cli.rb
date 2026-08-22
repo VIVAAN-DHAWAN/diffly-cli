c9cecf956ad93151dfbf6667fd2c0d8b6bbf1b04
Vivaan Dhawan <armaandhawan61@gmail.com>
2026-08-22 21:51:50 +0530

    Add PyPI trusted publishing workflow

--- homebrew/diffly-cli.rb
+++ homebrew/diffly-cli.rb
@@ -4,7 +4,7 @@ class DifflyCli < Formula
   desc "Deterministic triage for large GitHub pull requests"
   homepage "https://github.com/VIVAAN-DHAWAN/diffly-cli"
   url "https://files.pythonhosted.org/packages/source/d/diffly-cli/diffly-cli-0.4.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "8823266f2755196933141edaa72b269f33d873ac09bd637925a2923550e99877"
   license "MIT"
 
   depends_on "python@3.10" => :recommended
